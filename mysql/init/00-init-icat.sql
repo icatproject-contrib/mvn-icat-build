75a7c1b7dcb02c559bdce0cbd9f4476413b6e681
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2023-07-19 12:21:19 +0200

    Change the ICAT database password

--- mysql/init/00-init-icat.sql
+++ mysql/init/00-init-icat.sql
@@ -1,4 +1,4 @@
-CREATE USER 'icat'@'%' IDENTIFIED BY 'icat-db-secret';
+CREATE USER 'icat'@'%' IDENTIFIED BY 'secret-icatdb';
 CREATE DATABASE `icat`;
 CREATE DATABASE `icat_authn_db`;
 CREATE DATABASE `topcat`;
